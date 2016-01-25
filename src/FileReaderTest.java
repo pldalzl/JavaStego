fa750757690b809ccb4866a5aa0a7209ee4de5c1
paul <pldazl@gmail.com>
2016-01-25 20:58:42 +0000

    inital commit

---
+++ src/FileReaderTest.java
@@ -0,0 +1,52 @@
+import groovy.util.GroovyTestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by paul on 25/01/16.
+ */
+public class FileReaderTest extends GroovyTestCase {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testHasNextBit() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNextBit() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFileSize() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSuccessBool() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSizeBits() throws Exception {
+
+    }
+
+    @Test
+    public void testGetExtBits() throws Exception {
+
+    }
+}
---
+++ src/StegTest.java
@@ -0,0 +1,44 @@
+import groovy.util.GroovyTestCase;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by paul on 25/01/16.
+ */
+public class StegTest extends GroovyTestCase {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testHideString() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testExtractString() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testHideFile() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testExtractFile() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSwapLsb() throws Exception {
+
+    }
+}
